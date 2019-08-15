d960ba5d02c86c00f25c4b6cf67889ed03d3a48f
Tim van der Molen <tim@kariliq.nl>
2019-08-15 19:59:57 +0200

    Allow exporting messages as emails in a maildir.

--- cmd-messages.c
+++ cmd-messages.c
@@ -16,8 +16,11 @@
 
 #include <sys/types.h>
 #include <sys/queue.h>
+#include <sys/stat.h>
 
 #include <err.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,6 +30,231 @@
 
 #include "sbk.h"
 #include "sigbak.h"
+
+#define FORMAT_MAILDIR	0
+#define FORMAT_TEXT	1
+
+static void
+maildir_create(const char *path)
+{
+	int fd;
+
+	if (mkdir(path, 0777) == -1 && errno != EEXIST)
+		err(1, "mkdir: %s", path);
+
+	if ((fd = open(path, O_RDONLY | O_DIRECTORY)) == -1)
+		err(1, "open: %s", path);
+
+	if (mkdirat(fd, "cur", 0777) == -1 && errno != EEXIST)
+		err(1, "mkdirat: %s/%s", path, "cur");
+
+	if (mkdirat(fd, "new", 0777) == -1 && errno != EEXIST)
+		err(1, "mkdirat: %s/%s", path, "new");
+
+	if (mkdirat(fd, "tmp", 0777) == -1 && errno != EEXIST)
+		err(1, "mkdirat: %s/%s", path, "tmp");
+
+	close(fd);
+}
+
+static FILE *
+maildir_open_file(const char *maildir, int64_t date_recv, int64_t date_sent)
+{
+	FILE	*fp;
+	char	*path;
+
+	/* Intentionally create deterministic filenames */
+	/* XXX Shouldn't write directly into cur */
+	if (asprintf(&path, "%s/cur/%" PRId64 ".%" PRId64 ".localhost:2,S",
+	    maildir, date_recv, date_sent) == -1) {
+		warnx("asprintf() failed");
+		return NULL;
+	}
+
+	if ((fp = fopen(path, "wx")) == NULL)
+		warn("fopen: %s", path);
+
+	free(path);
+	return fp;
+}
+
+static void
+maildir_write_address_header(FILE *fp, const char *hdr, const char *addr,
+    const char *name)
+{
+	if (name == NULL)
+		fprintf(fp, "%s: %s@invalid\n", hdr, addr);
+	else
+		/* XXX Need to escape double quotes in name */
+		fprintf(fp, "%s: \"%s\" <%s@invalid>\n", hdr, name, addr);
+}
+
+static void
+maildir_write_date_header(FILE *fp, const char *hdr, int64_t date)
+{
+	const char	*days[] = {
+	    "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+	const char	*months[] = {
+	    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+	    "Oct", "Nov", "Dec" };
+
+	struct tm	*tm;
+	time_t		 tt;
+
+	tt = date / 1000;
+
+	if ((tm = localtime(&tt)) == NULL) {
+		warnx("localtime() failed");
+		return;
+	}
+
+	fprintf(fp, "%s: %s, %d %s %d %02d:%02d:%02d %c%02ld%02ld\n",
+	    hdr,
+	    days[tm->tm_wday],
+	    tm->tm_mday,
+	    months[tm->tm_mon],
+	    tm->tm_year + 1900,
+	    tm->tm_hour,
+	    tm->tm_min,
+	    tm->tm_sec,
+	    (tm->tm_gmtoff < 0) ? '-' : '+',
+	    labs(tm->tm_gmtoff) / 3600,
+	    labs(tm->tm_gmtoff) % 3600 / 60);
+}
+
+static int
+maildir_write_mms(struct sbk_ctx *ctx, const char *maildir,
+    struct sbk_mms *mms)
+{
+	FILE	*fp;
+	char	*name;
+	int	 isgroup;
+
+	if ((fp = maildir_open_file(maildir, mms->date_recv, mms->date_sent))
+	    == NULL)
+		return -1;
+
+	if (sbk_is_group_address(mms->address)) {
+		name = sbk_get_group_name(ctx, mms->address);
+		isgroup = 1;
+	} else {
+		name = sbk_get_contact_name(ctx, mms->address);
+		isgroup = 0;
+	}
+
+	if (SBK_IS_OUTGOING_MESSAGE(mms->type)) {
+		maildir_write_address_header(fp, "From", "you", "You");
+		maildir_write_address_header(fp, "To",
+		    isgroup ? "group" : mms->address, name);
+	} else {
+		maildir_write_address_header(fp, "From", mms->address, name);
+		maildir_write_address_header(fp, "To", "you", "You");
+	}
+
+	free(name);
+	maildir_write_date_header(fp, "Date", mms->date_sent);
+
+	if (!SBK_IS_OUTGOING_MESSAGE(mms->type))
+		maildir_write_date_header(fp, "X-Received", mms->date_recv);
+
+	fprintf(fp, "X-Type: %#x\n", mms->type);
+	fprintf(fp, "X-Thread: %d\n", mms->thread);
+	fputs("MIME-Version: 1.0\n", fp);
+	fputs("Content-Type: text/plain; charset=utf-8\n", fp);
+	fputs("Content-Disposition: inline\n", fp);
+
+	if (mms->body != NULL)
+		fprintf(fp, "\n%s\n", mms->body);
+
+	return 0;
+}
+
+static int
+maildir_write_sms(struct sbk_ctx *ctx, const char *maildir,
+    struct sbk_sms *sms)
+{
+	FILE	*fp;
+	char	*name;
+
+	if ((fp = maildir_open_file(maildir, sms->date_recv, sms->date_sent))
+	    == NULL)
+		return -1;
+
+	name = sbk_get_contact_name(ctx, sms->address);
+
+	if (SBK_IS_OUTGOING_MESSAGE(sms->type)) {
+		maildir_write_address_header(fp, "From", "you", "You");
+		maildir_write_address_header(fp, "To", sms->address, name);
+	} else {
+		maildir_write_address_header(fp, "From", sms->address, name);
+		maildir_write_address_header(fp, "To", "you", "You");
+	}
+
+	free(name);
+	maildir_write_date_header(fp, "Date", sms->date_sent);
+
+	if (!SBK_IS_OUTGOING_MESSAGE(sms->type))
+		maildir_write_date_header(fp, "X-Received", sms->date_recv);
+
+	fprintf(fp, "X-Type: %#x\n", sms->type);
+	fprintf(fp, "X-Thread: %d\n", sms->thread);
+	fputs("MIME-Version: 1.0\n", fp);
+	fputs("Content-Type: text/plain; charset=utf-8\n", fp);
+	fputs("Content-Disposition: inline\n", fp);
+
+	if (sms->body != NULL)
+		fprintf(fp, "\n%s\n", sms->body);
+
+	fclose(fp);
+	return 0;
+}
+
+static int
+maildir_write_messages(struct sbk_ctx *ctx, const char *maildir)
+{
+	struct sbk_mms_list	*mmslst;
+	struct sbk_sms_list	*smslst;
+	struct sbk_mms		*mms;
+	struct sbk_sms		*sms;
+
+	if ((mmslst = sbk_get_mmses(ctx)) == NULL) {
+		warnx("Cannot get mms messages: %s", sbk_error(ctx));
+		return -1;
+	}
+
+	if ((smslst = sbk_get_smses(ctx)) == NULL) {
+		warnx("Cannot get sms messages: %s", sbk_error(ctx));
+		sbk_free_mms_list(mmslst);
+		return -1;
+	}
+
+	mms = SIMPLEQ_FIRST(mmslst);
+	sms = SIMPLEQ_FIRST(smslst);
+
+	/* Print mms and sms messages in the order they were received */
+	for (;;)
+		if (mms == NULL) {
+			if (sms == NULL)
+				break; /* Done */
+			else {
+				maildir_write_sms(ctx, maildir, sms);
+				sms = SIMPLEQ_NEXT(sms, entries);
+			}
+		} else {
+			if (sms == NULL || mms->date_recv < sms->date_recv) {
+				maildir_write_mms(ctx, maildir, mms);
+				mms = SIMPLEQ_NEXT(mms, entries);
+			} else {
+				maildir_write_sms(ctx, maildir, sms);
+				sms = SIMPLEQ_NEXT(sms, entries);
+			}
+		}
+
+	sbk_free_mms_list(mmslst);
+	sbk_free_sms_list(smslst);
+	return 0;
+}
 
 static int
 text_write_mms(struct sbk_ctx *ctx, FILE *fp, struct sbk_mms *mms)
@@ -194,13 +422,22 @@ int
 cmd_messages(int argc, char **argv)
 {
 	struct sbk_ctx	*ctx;
-	char		*passfile, passphr[128], *outfile;
-	int		 c, ret;
+	char		*dest, *passfile, passphr[128];
+	int		 c, format, ret;
 
+	format = FORMAT_TEXT;
 	passfile = NULL;
 
-	while ((c = getopt(argc, argv, "p:")) != -1)
+	while ((c = getopt(argc, argv, "f:p:")) != -1)
 		switch (c) {
+		case 'f':
+			if (strcmp(optarg, "maildir") == 0)
+				format = FORMAT_MAILDIR;
+			else if (strcmp(optarg, "text") == 0)
+				format = FORMAT_TEXT;
+			else
+				errx(1, "%s: invalid format", optarg);
+			break;
 		case 'p':
 			passfile = optarg;
 			break;
@@ -213,11 +450,15 @@ cmd_messages(int argc, char **argv)
 
 	switch (argc) {
 	case 1:
-		outfile = NULL;
+		if (format == FORMAT_MAILDIR)
+			goto usage;
+		dest = NULL;
 		break;
 	case 2:
-		outfile = argv[1];
-		if (unveil(outfile, "wc") == -1)
+		dest = argv[1];
+		if (format == FORMAT_MAILDIR)
+			maildir_create(dest);
+		if (unveil(dest, "wc") == -1)
 			err(1, "unveil");
 		break;
 	default:
@@ -247,11 +488,20 @@ cmd_messages(int argc, char **argv)
 	}
 
 	explicit_bzero(passphr, sizeof passphr);
-	ret = text_write_messages(ctx, outfile);
+
+	switch (format) {
+	case FORMAT_MAILDIR:
+		ret = maildir_write_messages(ctx, dest);
+		break;
+	case FORMAT_TEXT:
+		ret = text_write_messages(ctx, dest);
+		break;
+	}
+
 	sbk_close(ctx);
 	sbk_ctx_free(ctx);
 	return (ret == 0) ? 0 : 1;
 
 usage:
-	usage("messages", "[-p passfile] backup [file]");
+	usage("messages", "[-f format] [-p passfile] backup dest");
 }
