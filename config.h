d10ba110900f2066f06823077d94195dd417a335
Tim van der Molen <tim@kariliq.nl>
2019-10-12 10:10:52 +0200

    Reorganise.

--- config.h
+++ config.h
@@ -1,126 +1,8 @@
-#ifdef __DragonFly__
-
 /*
- * DragonFly BSD (untested)
- */
-
-#define HAVE_ASPRINTF
-#define HAVE_ERR
-#define HAVE_FOPEN_X_MODE
-#define HAVE_FREEZERO
-#define HAVE_GETPAGESIZE
-#define HAVE_GETPROGNAME
-/* #define HAVE_PLEDGE */
-#define HAVE_READPASSPHRASE
-#define HAVE_REALLOCARRAY
-#define HAVE_RECALLOCARRAY
-/* #define HAVE_UNVEIL */
-#define HAVE___ATTRIBUTE__
-
-#define HAVE_EXPLICIT_BZERO
-/* #define HAVE_EXPLICIT_MEMSET */
-/* #define HAVE_MEMSET_S */
-
-#elif defined(__FreeBSD__)
-
-/*
- * FreeBSD
- */
-
-#define HAVE_ASPRINTF
-#define HAVE_ERR
-#define HAVE_FOPEN_X_MODE
-/* #define HAVE_FREEZERO */
-#define HAVE_GETPAGESIZE
-#define HAVE_GETPROGNAME
-/* #define HAVE_PLEDGE */
-#define HAVE_READPASSPHRASE
-#define HAVE_REALLOCARRAY
-/* #define HAVE_RECALLOCARRAY */
-/* #define HAVE_UNVEIL */
-#define HAVE___ATTRIBUTE__
-
-#define HAVE_EXPLICIT_BZERO
-/* #define HAVE_EXPLICIT_MEMSET */
-/* #define HAVE_MEMSET_S */
-
-#elif defined(__NetBSD__)
-
-/*
- * NetBSD
- */
-
-#define _OPENBSD_SOURCE
-
-#define HAVE_ASPRINTF
-#define HAVE_ERR
-#define HAVE_FOPEN_X_MODE
-/* #define HAVE_FREEZERO */
-#define HAVE_GETPAGESIZE
-#define HAVE_GETPROGNAME
-/* #define HAVE_PLEDGE */
-/* #define HAVE_READPASSPHRASE */
-#define HAVE_REALLOCARRAY
-/* #define HAVE_RECALLOCARRAY */
-/* #define HAVE_UNVEIL */
-#define HAVE___ATTRIBUTE__
-
-/* #define HAVE_EXPLICIT_BZERO */
-#define HAVE_EXPLICIT_MEMSET
-/* #define HAVE_MEMSET_S */
-
-#elif defined(__OpenBSD__)
-
-/*
- * OpenBSD
- */
-
-#define HAVE_ASPRINTF
-#define HAVE_ERR
-#define HAVE_FOPEN_X_MODE
-#define HAVE_FREEZERO
-#define HAVE_GETPAGESIZE
-#define HAVE_GETPROGNAME
-#define HAVE_PLEDGE
-#define HAVE_READPASSPHRASE
-#define HAVE_REALLOCARRAY
-#define HAVE_RECALLOCARRAY
-#define HAVE_UNVEIL
-#define HAVE___ATTRIBUTE__
-
-#define HAVE_EXPLICIT_BZERO
-/* #define HAVE_EXPLICIT_MEMSET */
-/* #define HAVE_MEMSET_S */
-
-#elif defined(__linux__)
-
-/*
- * Linux (tested on Debian 9.11 and 10.1)
- */
-
-#define _GNU_SOURCE
-
-#define HAVE_ASPRINTF
-#define HAVE_ERR
-#define HAVE_FOPEN_X_MODE
-/* #define HAVE_FREEZERO */
-#define HAVE_GETPAGESIZE
-/* #define HAVE_GETPROGNAME */
-/* #define HAVE_PLEDGE */
-/* #define HAVE_READPASSPHRASE */
-/* #define HAVE_REALLOCARRAY */
-/* #define HAVE_RECALLOCARRAY */
-/* #define HAVE_UNVEIL */
-#define HAVE___ATTRIBUTE__
-
-/* #define HAVE_EXPLICIT_BZERO */
-/* #define HAVE_EXPLICIT_MEMSET */
-/* #define HAVE_MEMSET_S */
-
-#else
-
-/*
- * Other
+ * This file must be suited to your system. Edit it if necessary.
+ *
+ * Several systems are already supported; see further below. On those systems,
+ * no editing should be necessary.
  */
 
 /* Define if you have asprintf() and vasprintf(). */
@@ -128,6 +10,12 @@
 
 /* Define if you have the err() family of functions. */
 /* #define HAVE_ERR */
+
+/* Define if you have explicit_bzero(). */
+/* #define HAVE_EXPLICIT_BZERO */
+
+/* Define if you have explicit_memset(). */
+/* #define HAVE_EXPLICIT_MEMSET */
 
 /* Define if your fopen() supports the "x" mode extension. */
 /* #define HAVE_FOPEN_X_MODE */
@@ -140,6 +28,9 @@
 
 /* Define if you have getprogname(). */
 /* #define HAVE_GETPROGNAME */
+
+/* Define if you have memset_s(). */
+/* #define HAVE_MEMSET_S */
 
 /* Define if you have pledge(). */
 /* #define HAVE_PLEDGE */
@@ -159,16 +50,73 @@
 /* Define if your compiler supports __attribute__(). */
 /* #define HAVE___ATTRIBUTE__ */
 
-/* Define if you have explicit_bzero(). */
-/* #define HAVE_EXPLICIT_BZERO */
+#ifdef __DragonFly__
 
-/*
- * Define if you don't have explicit_bzero(), but you do have
- * explicit_memset().
- */
-/* #define HAVE_EXPLICIT_MEMSET */
+/* Untested */
 
-/* Define if you don't have explicit_bzero(), but you do have memset_s(). */
-/* #define HAVE_MEMSET_S */
+#define HAVE_ASPRINTF
+#define HAVE_ERR
+#define HAVE_EXPLICIT_BZERO
+#define HAVE_FOPEN_X_MODE
+#define HAVE_FREEZERO
+#define HAVE_GETPAGESIZE
+#define HAVE_GETPROGNAME
+#define HAVE_READPASSPHRASE
+#define HAVE_REALLOCARRAY
+#define HAVE_RECALLOCARRAY
+#define HAVE___ATTRIBUTE__
+
+#elif defined(__FreeBSD__)
+
+#define HAVE_ASPRINTF
+#define HAVE_ERR
+#define HAVE_EXPLICIT_BZERO
+#define HAVE_FOPEN_X_MODE
+#define HAVE_GETPAGESIZE
+#define HAVE_GETPROGNAME
+#define HAVE_READPASSPHRASE
+#define HAVE_REALLOCARRAY
+#define HAVE___ATTRIBUTE__
+
+#elif defined(__NetBSD__)
+
+#define _OPENBSD_SOURCE
+
+#define HAVE_ASPRINTF
+#define HAVE_ERR
+#define HAVE_EXPLICIT_MEMSET
+#define HAVE_FOPEN_X_MODE
+#define HAVE_GETPAGESIZE
+#define HAVE_GETPROGNAME
+#define HAVE_REALLOCARRAY
+#define HAVE___ATTRIBUTE__
+
+#elif defined(__OpenBSD__)
+
+#define HAVE_ASPRINTF
+#define HAVE_ERR
+#define HAVE_EXPLICIT_BZERO
+#define HAVE_FOPEN_X_MODE
+#define HAVE_FREEZERO
+#define HAVE_GETPAGESIZE
+#define HAVE_GETPROGNAME
+#define HAVE_PLEDGE
+#define HAVE_READPASSPHRASE
+#define HAVE_REALLOCARRAY
+#define HAVE_RECALLOCARRAY
+#define HAVE_UNVEIL
+#define HAVE___ATTRIBUTE__
+
+#elif defined(__linux__)
+
+/* Tested on Debian 9.11 and 10.1 */
+
+#define _GNU_SOURCE
+
+#define HAVE_ASPRINTF
+#define HAVE_ERR
+#define HAVE_FOPEN_X_MODE
+#define HAVE_GETPAGESIZE
+#define HAVE___ATTRIBUTE__
 
 #endif
