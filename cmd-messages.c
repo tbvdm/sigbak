tbvdm/sigbak

commit 9aecccc4fa395bf4c6edb717eae6255257440ce4

Unify and simplify sms and mms handling

cmd-messages.c
@@ -124,39 +124,34 @@ maildir_write_date_header(FILE *fp, const char *hdr, int64_t date)
 }
 
 static int
-maildir_write_mms(struct sbk_ctx *ctx, const char *maildir,
-    struct sbk_mms *mms)
+maildir_write_message(struct sbk_ctx *ctx, const char *maildir,
+    struct sbk_message *msg)
 {
 	FILE	*fp;
 	char	*name, *phone;
 	int	 isgroup, ret;
 
 	ret = -1;
-	isgroup = sbk_is_group(ctx, mms->address);
+	isgroup = sbk_is_group(ctx, msg->address);
 
 	if (isgroup) {
-		if (sbk_get_group(ctx, mms->address, &name) == -1) {
+		if (sbk_get_group(ctx, msg->address, &name) == -1) {
 			warnx("%s", sbk_error(ctx));
 			return -1;
 		}
 		phone = NULL;
 	} else {
-		if (sbk_get_contact(ctx, mms->address, &name, &phone) == -1) {
+		if (sbk_get_contact(ctx, msg->address, &name, &phone) == -1) {
 			warnx("%s", sbk_error(ctx));
 			return -1;
 		}
 	}
 
-	if (sbk_get_long_message(ctx, mms) == -1) {
-		warnx("%s", sbk_error(ctx));
-		goto out;
-	}
-
-	if ((fp = maildir_open_file(maildir, mms->date_recv, mms->date_sent))
+	if ((fp = maildir_open_file(maildir, msg->time_recv, msg->time_sent))
 	    == NULL)
 		goto out;
 
-	if (SBK_IS_OUTGOING_MESSAGE(mms->type)) {
+	if (SBK_IS_OUTGOING_MESSAGE(msg->type)) {
 		maildir_write_address_header(fp, "From", "you", "You");
 		maildir_write_address_header(fp, "To",
 		    isgroup ? "group" : phone, name);
@@ -165,71 +160,18 @@ maildir_write_mms(struct sbk_ctx *ctx, const char *maildir,
 		maildir_write_address_header(fp, "To", "you", "You");
 	}
 
-	maildir_write_date_header(fp, "Date", mms->date_sent);
+	maildir_write_date_header(fp, "Date", msg->time_sent);
 
-	if (!SBK_IS_OUTGOING_MESSAGE(mms->type))
-		maildir_write_date_header(fp, "X-Received", mms->date_recv);
+	if (!SBK_IS_OUTGOING_MESSAGE(msg->type))
+		maildir_write_date_header(fp, "X-Received", msg->time_recv);
 
-	fprintf(fp, "X-Thread: %d\n", mms->thread);
+	fprintf(fp, "X-Thread: %d\n", msg->thread);
 	fputs("MIME-Version: 1.0\n", fp);
 	fputs("Content-Type: text/plain; charset=utf-8\n", fp);
 	fputs("Content-Disposition: inline\n", fp);
 
-	if (mms->body != NULL)
-		fprintf(fp, "\n%s\n", mms->body);
-
-	fclose(fp);
-	ret = 0;
-
-out:
-	freezero_string(name);
-	freezero_string(phone);
-	return ret;
-}
-
-static int
-maildir_write_sms(struct sbk_ctx *ctx, const char *maildir,
-    struct sbk_sms *sms)
-{
-	FILE	*fp;
-	char	*name, *phone;
-	int	 ret;
-
-	ret = -1;
-
-	if (sbk_get_contact(ctx, sms->address, &name, &phone) == -1) {
-		warnx("%s", sbk_error(ctx));
-		return -1;
-	}
-
-	if ((fp = maildir_open_file(maildir, sms->date_recv, sms->date_sent))
-	    == NULL)
-		goto out;
-
-	if (SBK_IS_OUTGOING_MESSAGE(sms->type)) {
-		maildir_write_address_header(fp, "From", "you", "You");
-		maildir_write_address_header(fp, "To",
-		    (phone != NULL) ? phone : "unknown",
-		    (name != NULL) ? name : "unknown");
-	} else {
-		maildir_write_address_header(fp, "From",
-		    (phone != NULL) ? phone : "unknown",
-		    (name != NULL) ? name : "unknown");
-		maildir_write_address_header(fp, "To", "you", "You");
-	}
-
-	maildir_write_date_header(fp, "Date", sms->date_sent);
-
-	if (!SBK_IS_OUTGOING_MESSAGE(sms->type))
-		maildir_write_date_header(fp, "X-Received", sms->date_recv);
-
-	fprintf(fp, "X-Thread: %d\n", sms->thread);
-	fputs("MIME-Version: 1.0\n", fp);
-	fputs("Content-Type: text/plain; charset=utf-8\n", fp);
-	fputs("Content-Disposition: inline\n", fp);
-
-	if (sms->body != NULL)
-		fprintf(fp, "\n%s\n", sms->body);
+	if (msg->text != NULL)
+		fprintf(fp, "\n%s\n", msg->text);
 
 	fclose(fp);
 	ret = 0;
@@ -243,74 +185,53 @@ out:
 static int
 maildir_write_messages(struct sbk_ctx *ctx, const char *maildir, int thread)
 {
-	struct sbk_mms_list	*mmslst;
-	struct sbk_sms_list	*smslst;
-	struct sbk_mms		*mms;
-	struct sbk_sms		*sms;
+	struct sbk_message_list	*lst;
+	struct sbk_message	*msg;
 	int			 ret;
 
-	if ((mmslst = sbk_get_mmses(ctx, thread)) == NULL) {
-		warnx("Cannot get mms messages: %s", sbk_error(ctx));
+	if (thread == -1)
+		lst = sbk_get_all_messages(ctx);
+	else
+		lst = sbk_get_messages_for_thread(ctx, thread);
+
+	if (lst == NULL) {
+		warnx("Cannot get messages: %s", sbk_error(ctx));
 		return -1;
 	}
 
-	if ((smslst = sbk_get_smses(ctx, thread)) == NULL) {
-		warnx("Cannot get sms messages: %s", sbk_error(ctx));
-		sbk_free_mms_list(mmslst);
-		return -1;
-	}
-
-	mms = SIMPLEQ_FIRST(mmslst);
-	sms = SIMPLEQ_FIRST(smslst);
 	ret = 0;
 
-	/* Print mms and sms messages in the order they were received */
-	for (;;)
-		if (mms == NULL) {
-			if (sms == NULL)
-				break; /* Done */
-			else {
-				ret |= maildir_write_sms(ctx, maildir, sms);
-				sms = SIMPLEQ_NEXT(sms, entries);
-			}
-		} else {
-			if (sms == NULL || mms->date_recv < sms->date_recv) {
-				ret |= maildir_write_mms(ctx, maildir, mms);
-				mms = SIMPLEQ_NEXT(mms, entries);
-			} else {
-				ret |= maildir_write_sms(ctx, maildir, sms);
-				sms = SIMPLEQ_NEXT(sms, entries);
-			}
-		}
+	SIMPLEQ_FOREACH(msg, lst, entries)
+		if (maildir_write_message(ctx, maildir, msg) == -1)
+			ret = -1;
 
-	sbk_free_mms_list(mmslst);
-	sbk_free_sms_list(smslst);
-	return (ret == 0) ? 0 : -1;
+	sbk_free_message_list(lst);
+	return ret;
 }
 
 static int
-text_write_mms(struct sbk_ctx *ctx, FILE *fp, struct sbk_mms *mms)
+text_write_message(struct sbk_ctx *ctx, FILE *fp, struct sbk_message *msg)
 {
-	struct sbk_attachment *att;
-	char	*name, *phone;
-	int	 isgroup;
+	struct sbk_attachment	*att;
+	char			*name, *phone;
+	int			 isgroup;
 
-	isgroup = sbk_is_group(ctx, mms->address);
+	isgroup = sbk_is_group(ctx, msg->address);
 
 	if (isgroup) {
-		if (sbk_get_group(ctx, mms->address, &name) == -1) {
+		if (sbk_get_group(ctx, msg->address, &name) == -1) {
 			warnx("%s", sbk_error(ctx));
 			return -1;
 		}
 		phone = NULL;
 	} else {
-		if (sbk_get_contact(ctx, mms->address, &name, &phone) == -1) {
+		if (sbk_get_contact(ctx, msg->address, &name, &phone) == -1) {
 			warnx("%s", sbk_error(ctx));
 			return -1;
 		}
 	}
 
-	if (SBK_IS_OUTGOING_MESSAGE(mms->type))
+	if (SBK_IS_OUTGOING_MESSAGE(msg->type))
 		fputs("To: ", fp);
 	else
 		fputs("From: ", fp);
@@ -319,91 +240,44 @@ text_write_mms(struct sbk_ctx *ctx, FILE *fp, struct sbk_mms *mms)
 	    (name != NULL) ? name : "unknown",
 	    isgroup ? "group" : ((phone != NULL) ? phone : "unknown"));
 
-	maildir_write_date_header(fp, "Sent", mms->date_sent);
+	maildir_write_date_header(fp, "Sent", msg->time_sent);
 
-	if (!SBK_IS_OUTGOING_MESSAGE(mms->type))
-		maildir_write_date_header(fp, "Received", mms->date_recv);
+	if (!SBK_IS_OUTGOING_MESSAGE(msg->type))
+		maildir_write_date_header(fp, "Received", msg->time_recv);
 
-	fprintf(fp, "Thread: %d\n", mms->thread);
+	fprintf(fp, "Thread: %d\n", msg->thread);
 
-	if (mms->nattachments > 0) {
-		if (sbk_get_mms_attachments(ctx, mms) == -1) {
-			warnx("%s", sbk_error(ctx));
-			return -1;
-		}
-
-		SIMPLEQ_FOREACH(att, mms->attachments, entries) {
+	if (msg->attachments != NULL)
+		SIMPLEQ_FOREACH(att, msg->attachments, entries) {
 			fputs("Attachment: ", fp);
+
 			if (att->filename == NULL)
 				fputs("no filename", fp);
 			else
 				fprintf(fp, "\"%s\"", att->filename);
+
 			fprintf(fp, " (%s, %" PRIu64 " bytes, id %" PRId64
-			    "-%" PRId64 ")\n", (att->content_type != NULL) ?
-			    att->content_type : "", att->size, att->rowid,
+			    "-%" PRId64 ")\n",
+			    (att->content_type != NULL) ?
+			    att->content_type : "",
+			    att->size,
+			    att->rowid,
 			    att->attachmentid);
 		}
-	}
 
-	if (sbk_get_long_message(ctx, mms) == -1) {
-		warnx("%s", sbk_error(ctx));
-		return -1;
-	}
-
-	if (mms->body != NULL)
-		fprintf(fp, "\n%s\n\n", mms->body);
+	if (msg->text != NULL)
+		fprintf(fp, "\n%s\n\n", msg->text);
 	else
 		fputs("\n", fp);
 
-	freezero_string(name);
-	freezero_string(phone);
-
-	return 0;
-}
-
-static int
-text_write_sms(struct sbk_ctx *ctx, FILE *fp, struct sbk_sms *sms)
-{
-	char *name, *phone;
-
-	if (sbk_get_contact(ctx, sms->address, &name, &phone) == -1) {
-		warnx("%s", sbk_error(ctx));
-		return -1;
-	}
-
-	if (SBK_IS_OUTGOING_MESSAGE(sms->type))
-		fputs("To: ", fp);
-	else
-		fputs("From: ", fp);
-
-	fprintf(fp, "%s (%s)\n",
-	    (name != NULL) ? name : "unknown",
-	    (phone != NULL) ? phone : "unknown");
-
-	maildir_write_date_header(fp, "Sent", sms->date_sent);
-
-	if (!SBK_IS_OUTGOING_MESSAGE(sms->type))
-		maildir_write_date_header(fp, "Received", sms->date_recv);
-
-	fprintf(fp, "Thread: %d\n", sms->thread);
-
-	if (sms->body != NULL)
-		fprintf(fp, "\n%s\n\n", sms->body);
-	else
-		fputs("\n", fp);
-
-	freezero_string(name);
-	freezero_string(phone);
 	return 0;
 }
 
 static int
 text_write_messages(struct sbk_ctx *ctx, const char *outfile, int thread)
 {
-	struct sbk_mms_list	*mmslst;
-	struct sbk_sms_list	*smslst;
-	struct sbk_mms		*mms;
-	struct sbk_sms		*sms;
+	struct sbk_message_list	*lst;
+	struct sbk_message	*msg;
 	FILE			*fp;
 	int			 ret;
 
@@ -414,47 +288,29 @@ text_write_messages(struct sbk_ctx *ctx, const char *outfile, int thread)
 		return -1;
 	}
 
-	if ((mmslst = sbk_get_mmses(ctx, thread)) == NULL) {
-		warnx("Cannot get mms messages: %s", sbk_error(ctx));
+	if (thread == -1)
+		lst = sbk_get_all_messages(ctx);
+	else
+		lst = sbk_get_messages_for_thread(ctx, thread);
+
+	if (lst == NULL) {
+		warnx("Cannot get messages: %s", sbk_error(ctx));
+		fclose(fp);
 		return -1;
 	}
 
-	if ((smslst = sbk_get_smses(ctx, thread)) == NULL) {
-		warnx("Cannot get sms messages: %s", sbk_error(ctx));
-		sbk_free_mms_list(mmslst);
-		return -1;
-	}
-
-	mms = SIMPLEQ_FIRST(mmslst);
-	sms = SIMPLEQ_FIRST(smslst);
 	ret = 0;
 
-	/* Print mms and sms messages in the order they were received */
-	for (;;)
-		if (mms == NULL) {
-			if (sms == NULL)
-				break; /* Done */
-			else {
-				ret |= text_write_sms(ctx, fp, sms);
-				sms = SIMPLEQ_NEXT(sms, entries);
-			}
-		} else {
-			if (sms == NULL || mms->date_recv < sms->date_recv) {
-				ret |= text_write_mms(ctx, fp, mms);
-				mms = SIMPLEQ_NEXT(mms, entries);
-			} else {
-				ret |= text_write_sms(ctx, fp, sms);
-				sms = SIMPLEQ_NEXT(sms, entries);
-			}
-		}
+	SIMPLEQ_FOREACH(msg, lst, entries)
+		if (text_write_message(ctx, fp, msg) == -1)
+			ret = -1;
 
-	sbk_free_mms_list(mmslst);
-	sbk_free_sms_list(smslst);
+	sbk_free_message_list(lst);
 
 	if (fp != stdout)
 		fclose(fp);
 
-	return (ret == 0) ? 0 : -1;
+	return ret;
 }
 
 int

sbk.c
@@ -1057,64 +1057,64 @@ error:
 	return -1;
 }
 
-static void
-sbk_get_body(struct sbk_ctx *ctx, char **body, int type, const char *address)
+static int
+sbk_get_body(struct sbk_ctx *ctx, struct sbk_message *msg)
 {
 	char		*name;
 	const char	*contact, *fmt;
 
 	fmt = NULL;
 
-	if (type & SBK_ENCRYPTION_REMOTE_FAILED_BIT)
+	if (msg->type & SBK_ENCRYPTION_REMOTE_FAILED_BIT)
 		fmt = "Bad encrypted message";
-	else if (type & SBK_ENCRYPTION_REMOTE_NO_SESSION_BIT)
+	else if (msg->type & SBK_ENCRYPTION_REMOTE_NO_SESSION_BIT)
 		fmt = "Message encrypted for non-existing session";
-	else if (type & SBK_ENCRYPTION_REMOTE_DUPLICATE_BIT)
+	else if (msg->type & SBK_ENCRYPTION_REMOTE_DUPLICATE_BIT)
 		fmt = "Duplicate message";
-	else if ((type & SBK_ENCRYPTION_REMOTE_LEGACY_BIT) ||
-	    (type & SBK_ENCRYPTION_REMOTE_BIT))
+	else if ((msg->type & SBK_ENCRYPTION_REMOTE_LEGACY_BIT) ||
+	    (msg->type & SBK_ENCRYPTION_REMOTE_BIT))
 		fmt = "Encrypted message sent from an older version of Signal "
 		    "that is no longer supported";
-	else if (type & SBK_GROUP_UPDATE_BIT) {
-		if (SBK_IS_OUTGOING_MESSAGE(type))
+	else if (msg->type & SBK_GROUP_UPDATE_BIT) {
+		if (SBK_IS_OUTGOING_MESSAGE(msg->type))
 			fmt = "You updated the group";
 		else
 			fmt = "%s updated the group";
-	} else if (type & SBK_GROUP_QUIT_BIT) {
-		if (SBK_IS_OUTGOING_MESSAGE(type))
+	} else if (msg->type & SBK_GROUP_QUIT_BIT) {
+		if (SBK_IS_OUTGOING_MESSAGE(msg->type))
 			fmt = "You have left the group";
 		else
 			fmt = "%s has left the group";
-	} else if (type & SBK_END_SESSION_BIT) {
-		if (SBK_IS_OUTGOING_MESSAGE(type))
+	} else if (msg->type & SBK_END_SESSION_BIT) {
+		if (SBK_IS_OUTGOING_MESSAGE(msg->type))
 			fmt = "You reset the secure session";
 		else
 			fmt = "%s reset the secure session";
-	} else if (type & SBK_KEY_EXCHANGE_IDENTITY_VERIFIED_BIT) {
-		if (SBK_IS_OUTGOING_MESSAGE(type))
+	} else if (msg->type & SBK_KEY_EXCHANGE_IDENTITY_VERIFIED_BIT) {
+		if (SBK_IS_OUTGOING_MESSAGE(msg->type))
 			fmt = "You marked your safety number with %s verified";
 		else
 			fmt = "You marked your safety number with %s verified "
 			    "from another device";
-	} else if (type & SBK_KEY_EXCHANGE_IDENTITY_DEFAULT_BIT) {
-		if (SBK_IS_OUTGOING_MESSAGE(type))
+	} else if (msg->type & SBK_KEY_EXCHANGE_IDENTITY_DEFAULT_BIT) {
+		if (SBK_IS_OUTGOING_MESSAGE(msg->type))
 			fmt = "You marked your safety number with %s "
 			    "unverified";
 		else
 			fmt = "You marked your safety number with %s "
 			    "unverified from another device";
-	} else if (type & SBK_KEY_EXCHANGE_CORRUPTED_BIT)
+	} else if (msg->type & SBK_KEY_EXCHANGE_CORRUPTED_BIT)
 		fmt = "Corrupt key exchange message";
-	else if (type & SBK_KEY_EXCHANGE_INVALID_VERSION_BIT)
+	else if (msg->type & SBK_KEY_EXCHANGE_INVALID_VERSION_BIT)
 		fmt = "Key exchange message for invalid protocol version";
-	else if (type & SBK_KEY_EXCHANGE_BUNDLE_BIT)
+	else if (msg->type & SBK_KEY_EXCHANGE_BUNDLE_BIT)
 		fmt = "Message with new safety number";
-	else if (type & SBK_KEY_EXCHANGE_IDENTITY_UPDATE_BIT)
+	else if (msg->type & SBK_KEY_EXCHANGE_IDENTITY_UPDATE_BIT)
 		fmt = "Your safety number with %s has changed";
-	else if (type & SBK_KEY_EXCHANGE_BIT)
+	else if (msg->type & SBK_KEY_EXCHANGE_BIT)
 		fmt = "Key exchange message";
 	else
-		switch (type & SBK_BASE_TYPE_MASK) {
+		switch (msg->type & SBK_BASE_TYPE_MASK) {
 		case SBK_INCOMING_CALL_TYPE:
 			fmt = "%s called you";
 			break;
@@ -1140,123 +1140,24 @@ sbk_get_body(struct sbk_ctx *ctx, char **body, int type, const char *address)
 		}
 
 	if (fmt == NULL)
-		return;
+		return 0;
 
-	if (sbk_get_contact(ctx, address, &name, NULL) == -1 ||
+	if (sbk_get_contact(ctx, msg->address, &name, NULL) == -1 ||
 	    name == NULL)
-		contact = address;
+		contact = msg->address;
 	else
 		contact = name;
 
-	freezero_string(*body);
+	freezero_string(msg->text);
 
-	if (asprintf(body, fmt, contact) == -1)
-		*body = NULL;
+	if (asprintf(&msg->text, fmt, contact) == -1) {
+		msg->text = NULL;
+		free(name);
+		return -1;
+	}
 
 	free(name);
-}
-
-static void
-sbk_get_sms_body(struct sbk_ctx *ctx, struct sbk_sms *sms)
-{
-	sbk_get_body(ctx, &sms->body, sms->type, sms->address);
-}
-
-static void
-sbk_free_sms(struct sbk_sms *sms)
-{
-	freezero_string(sms->address);
-	freezero_string(sms->body);
-	freezero(sms, sizeof *sms);
-}
-
-void
-sbk_free_sms_list(struct sbk_sms_list *lst)
-{
-	struct sbk_sms *sms;
-
-	if (lst != NULL) {
-		while ((sms = SIMPLEQ_FIRST(lst)) != NULL) {
-			SIMPLEQ_REMOVE_HEAD(lst, entries);
-			sbk_free_sms(sms);
-		}
-		free(lst);
-	}
-}
-
-struct sbk_sms_list *
-sbk_get_smses(struct sbk_ctx *ctx, int thread)
-{
-	struct sbk_sms_list	*lst;
-	struct sbk_sms		*sms;
-	sqlite3_stmt		*stm;
-	int			 ret;
-
-	if (sbk_create_database(ctx) == -1)
-		return NULL;
-
-	if ((lst = malloc(sizeof *lst)) == NULL) {
-		sbk_error_set(ctx, NULL);
-		return NULL;
-	}
-
-	SIMPLEQ_INIT(lst);
-
-	if (thread == -1) {
-		if (sbk_sqlite_prepare(ctx, &stm, "SELECT address, body, _id, "
-		    "date, date_sent, thread_id, type FROM sms ORDER BY date")
-		    == -1)
-			goto error;
-	} else {
-		if (sbk_sqlite_prepare(ctx, &stm, "SELECT address, body, _id, "
-		    "date, date_sent, thread_id, type FROM sms WHERE "
-		    "thread_id = ? ORDER BY date") == -1)
-			goto error;
-
-		if (sbk_sqlite_bind_int(ctx, stm, 1, thread) == -1)
-			goto error;
-	}
-
-	while ((ret = sbk_sqlite_step(ctx, stm)) == SQLITE_ROW) {
-		if ((sms = malloc(sizeof *sms)) == NULL) {
-			sbk_error_set(ctx, NULL);
-			goto error;
-		}
-
-		sms->address = NULL;
-		sms->body = NULL;
-
-		if (sbk_sqlite_column_text_copy(ctx, &sms->address, stm, 0) ==
-		    -1) {
-			sbk_free_sms(sms);
-			goto error;
-		}
-
-		if (sbk_sqlite_column_text_copy(ctx, &sms->body, stm, 1) ==
-		    -1) {
-			sbk_free_sms(sms);
-			goto error;
-		}
-
-		sms->id = sqlite3_column_int(stm, 2);
-		sms->date_recv = sqlite3_column_int64(stm, 3);
-		sms->date_sent = sqlite3_column_int64(stm, 4);
-		sms->thread = sqlite3_column_int(stm, 5);
-		sms->type = sqlite3_column_int(stm, 6);
-		sbk_get_sms_body(ctx, sms);
-		SIMPLEQ_INSERT_TAIL(lst, sms, entries);
-	}
-
-	if (ret != SQLITE_DONE)
-		goto error;
-
-	sqlite3_finalize(stm);
-	return lst;
-
-error:
-	sbk_free_sms_list(lst);
-	sqlite3_finalize(stm);
-	return NULL;
+	return 0;
 }
 
 static void
@@ -1418,158 +1319,38 @@ sbk_get_attachments_for_thread(struct sbk_ctx *ctx, int thread_id)
 	return sbk_get_attachments(ctx, stm);
 }
 
-struct sbk_attachment_list *
-sbk_get_attachments_for_mms(struct sbk_ctx *ctx, int mms_id)
+static int
+sbk_get_attachments_for_message(struct sbk_ctx *ctx, struct sbk_message *msg,
+    int mms_id)
 {
 	sqlite3_stmt *stm;
-
-	if (sbk_create_database(ctx) == -1)
-		return NULL;
 
 	if (sbk_sqlite_prepare(ctx, &stm, SBK_ATTACHMENTS_QUERY_MESSAGE) ==
 	    -1) {
 		sqlite3_finalize(stm);
-		return NULL;
+		return -1;
 	}
 
 	if (sbk_sqlite_bind_int(ctx, stm, 1, mms_id) == -1) {
 		sqlite3_finalize(stm);
-		return NULL;
+		return -1;
 	}
 
-	return sbk_get_attachments(ctx, stm);
-}
-
-int
-sbk_get_mms_attachments(struct sbk_ctx *ctx, struct sbk_mms *mms)
-{
-	if (mms->attachments == NULL) {
-		mms->attachments = sbk_get_attachments_for_mms(ctx, mms->id);
-		if (mms->attachments == NULL)
-			return -1;
-	}
+	if ((msg->attachments = sbk_get_attachments(ctx, stm)) == NULL)
+		return -1;
 
 	return 0;
 }
 
-static void
-sbk_get_mms_body(struct sbk_ctx *ctx, struct sbk_mms *mms)
-{
-	sbk_get_body(ctx, &mms->body, mms->type, mms->address);
-}
-
-static void
-sbk_free_mms(struct sbk_mms *mms)
-{
-	freezero_string(mms->address);
-	freezero_string(mms->body);
-	sbk_free_attachment_list(mms->attachments);
-	freezero(mms, sizeof *mms);
-}
-
-void
-sbk_free_mms_list(struct sbk_mms_list *lst)
-{
-	struct sbk_mms *mms;
-
-	if (lst != NULL) {
-		while ((mms = SIMPLEQ_FIRST(lst)) != NULL) {
-			SIMPLEQ_REMOVE_HEAD(lst, entries);
-			sbk_free_mms(mms);
-		}
-		free(lst);
-	}
-}
-
-struct sbk_mms_list *
-sbk_get_mmses(struct sbk_ctx *ctx, int thread)
-{
-	struct sbk_mms_list	*lst;
-	struct sbk_mms		*mms;
-	sqlite3_stmt		*stm;
-	int			 ret;
-
-	if (sbk_create_database(ctx) == -1)
-		return NULL;
-
-	if ((lst = malloc(sizeof *lst)) == NULL) {
-		sbk_error_set(ctx, NULL);
-		return NULL;
-	}
-
-	SIMPLEQ_INIT(lst);
-
-	if (thread == -1) {
-		if (sbk_sqlite_prepare(ctx, &stm, "SELECT address, body, _id, "
-		    "date_received, date, thread_id, msg_box, part_count FROM "
-		    "mms ORDER BY date_received") == -1)
-			goto error;
-	} else {
-		if (sbk_sqlite_prepare(ctx, &stm, "SELECT address, body, _id, "
-		    "date_received, date, thread_id, msg_box, part_count FROM "
-		    "mms WHERE thread_id = ? ORDER BY date_received") == -1)
-			goto error;
-
-		if (sbk_sqlite_bind_int(ctx, stm, 1, thread) == -1)
-			goto error;
-	}
-
-	while ((ret = sbk_sqlite_step(ctx, stm)) == SQLITE_ROW) {
-		if ((mms = malloc(sizeof *mms)) == NULL) {
-			sbk_error_set(ctx, NULL);
-			goto error;
-		}
-
-		mms->address = NULL;
-		mms->body = NULL;
-		mms->attachments = NULL;
-
-		if (sbk_sqlite_column_text_copy(ctx, &mms->address, stm, 0) ==
-		    -1) {
-			sbk_free_mms(mms);
-			goto error;
-		}
-
-		if (sbk_sqlite_column_text_copy(ctx, &mms->body, stm, 1) ==
-		    -1) {
-			sbk_free_mms(mms);
-			goto error;
-		}
-
-		mms->id = sqlite3_column_int(stm, 2);
-		mms->date_recv = sqlite3_column_int64(stm, 3);
-		mms->date_sent = sqlite3_column_int64(stm, 4);
-		mms->thread = sqlite3_column_int(stm, 5);
-		mms->type = sqlite3_column_int(stm, 6);
-		mms->nattachments = sqlite3_column_int(stm, 7);
-		sbk_get_mms_body(ctx, mms);
-		SIMPLEQ_INSERT_TAIL(lst, mms, entries);
-	}
-
-	if (ret != SQLITE_DONE)
-		goto error;
-
-	sqlite3_finalize(stm);
-	return lst;
-
-error:
-	sbk_free_mms_list(lst);
-	sqlite3_finalize(stm);
-	return NULL;
-}
-
-int
-sbk_get_long_message(struct sbk_ctx *ctx, struct sbk_mms *mms)
+static int
+sbk_get_long_message(struct sbk_ctx *ctx, struct sbk_message *msg)
 {
 	struct sbk_attachment	*att;
 	char			*longmsg;
 	int			 found;
 
-	if (sbk_get_mms_attachments(ctx, mms) == -1)
-		return -1;
-
 	found = 0;
-	SIMPLEQ_FOREACH(att, mms->attachments, entries)
+	SIMPLEQ_FOREACH(att, msg->attachments, entries)
 		if (att->content_type != NULL &&
 		    strcmp(att->content_type, SBK_LONG_TEXT_TYPE) == 0) {
 			found = 1;
@@ -1587,9 +1368,178 @@ sbk_get_long_message(struct sbk_ctx *ctx, struct sbk_mms *mms)
 	if ((longmsg = sbk_get_file_as_string(ctx, att->file)) == NULL)
 		return -1;
 
-	freezero_string(mms->body);
-	mms->body = longmsg;
+	freezero_string(msg->text);
+	msg->text = longmsg;
 	return 0;
+}
+
+static void
+sbk_free_message(struct sbk_message *msg)
+{
+	freezero_string(msg->address);
+	freezero_string(msg->text);
+	sbk_free_attachment_list(msg->attachments);
+	freezero(msg, sizeof *msg);
+}
+
+void
+sbk_free_message_list(struct sbk_message_list *lst)
+{
+	struct sbk_message *msg;
+
+	if (lst != NULL) {
+		while ((msg = SIMPLEQ_FIRST(lst)) != NULL) {
+			SIMPLEQ_REMOVE_HEAD(lst, entries);
+			sbk_free_message(msg);
+		}
+		free(lst);
+	}
+}
+
+#define SBK_MESSAGES_SELECT_SMS						\
+	"SELECT address, body, date_sent, date AS date_received, "	\
+	"type, thread_id, 0 AS part_count, _id FROM sms "
+
+#define SBK_MESSAGES_SELECT_MMS						\
+	"SELECT address, body, date AS date_sent, date_received, "	\
+	"msg_box AS type, thread_id, part_count, _id FROM mms "
+
+#define SBK_MESSAGES_WHERE_THREAD	"WHERE thread_id = ? "
+
+#define SBK_MESSAGES_ORDER		"ORDER BY date_received"
+
+#define SBK_MESSAGES_QUERY_ALL						\
+	SBK_MESSAGES_SELECT_SMS						\
+	"UNION ALL "							\
+	SBK_MESSAGES_SELECT_MMS						\
+	SBK_MESSAGES_ORDER
+
+#define SBK_MESSAGES_QUERY_THREAD					\
+	SBK_MESSAGES_SELECT_SMS	SBK_MESSAGES_WHERE_THREAD		\
+	"UNION ALL "							\
+	SBK_MESSAGES_SELECT_MMS SBK_MESSAGES_WHERE_THREAD		\
+	SBK_MESSAGES_ORDER
+
+static struct sbk_message *
+sbk_get_message(struct sbk_ctx *ctx, sqlite3_stmt *stm)
+{
+	struct sbk_message	*msg;
+	int			 id, nattachments;
+
+	if ((msg = malloc(sizeof *msg)) == NULL) {
+		sbk_error_set(ctx, NULL);
+		return NULL;
+	}
+
+	msg->address = NULL;
+	msg->text = NULL;
+	msg->attachments = NULL;
+
+	if (sbk_sqlite_column_text_copy(ctx, &msg->address, stm, 0) == -1)
+		goto error;
+
+	if (sbk_sqlite_column_text_copy(ctx, &msg->text, stm, 1) == -1)
+		goto error;
+
+	msg->time_sent = sqlite3_column_int64(stm, 2);
+	msg->time_recv = sqlite3_column_int64(stm, 3);
+	msg->type = sqlite3_column_int(stm, 4);
+	msg->thread = sqlite3_column_int(stm, 5);
+
+	if (sbk_get_body(ctx, msg) == -1)
+		goto error;
+
+	nattachments = sqlite3_column_int(stm, 6);
+
+	if (nattachments > 0) {
+		id = sqlite3_column_int(stm, 7);
+
+		if (sbk_get_attachments_for_message(ctx, msg, id) == -1)
+			goto error;
+
+		if (sbk_get_long_message(ctx, msg) == -1)
+			goto error;
+	}
+
+	return msg;
+
+error:
+	sbk_free_message(msg);
+	return NULL;
+}
+
+struct sbk_message_list *
+sbk_get_messages(struct sbk_ctx *ctx, sqlite3_stmt *stm)
+{
+	struct sbk_message_list	*lst;
+	struct sbk_message	*msg;
+	int			 ret;
+
+	if ((lst = malloc(sizeof *lst)) == NULL) {
+		sbk_error_set(ctx, NULL);
+		goto error;
+	}
+
+	SIMPLEQ_INIT(lst);
+
+	while ((ret = sbk_sqlite_step(ctx, stm)) == SQLITE_ROW) {
+		if ((msg = sbk_get_message(ctx, stm)) == NULL)
+			goto error;
+		SIMPLEQ_INSERT_TAIL(lst, msg, entries);
+	}
+
+	if (ret != SQLITE_DONE)
+		goto error;
+
+	sqlite3_finalize(stm);
+	return lst;
+
+error:
+	sbk_free_message_list(lst);
+	sqlite3_finalize(stm);
+	return NULL;
+}
+
+struct sbk_message_list *
+sbk_get_all_messages(struct sbk_ctx *ctx)
+{
+	sqlite3_stmt *stm;
+
+	if (sbk_create_database(ctx) == -1)
+		return NULL;
+
+	if (sbk_sqlite_prepare(ctx, &stm, SBK_MESSAGES_QUERY_ALL) == -1) {
+		sqlite3_finalize(stm);
+		return NULL;
+	}
+
+	return sbk_get_messages(ctx, stm);
+}
+
+struct sbk_message_list *
+sbk_get_messages_for_thread(struct sbk_ctx *ctx, int thread_id)
+{
+	sqlite3_stmt *stm;
+
+	if (sbk_create_database(ctx) == -1)
+		return NULL;
+
+	if (sbk_sqlite_prepare(ctx, &stm, SBK_MESSAGES_QUERY_THREAD) == -1) {
+		sqlite3_finalize(stm);
+		return NULL;
+	}
+
+	if (sbk_sqlite_bind_int(ctx, stm, 1, thread_id) == -1) {
+		sqlite3_finalize(stm);
+		return NULL;
+	}
+
+	if (sbk_sqlite_bind_int(ctx, stm, 2, thread_id) == -1) {
+		sqlite3_finalize(stm);
+		return NULL;
+	}
+
+	return sbk_get_messages(ctx, stm);
 }
 
 void

sigbak.h
@@ -111,19 +111,6 @@ struct sbk_ctx;
 
 struct sbk_file;
 
-struct sbk_sms {
-	int		 id;
-	char		*address;
-	uint64_t	 date_recv;
-	uint64_t	 date_sent;
-	int		 thread;
-	int		 type;
-	char		*body;
-	SIMPLEQ_ENTRY(sbk_sms) entries;
-};
-
-SIMPLEQ_HEAD(sbk_sms_list, sbk_sms);
-
 struct sbk_attachment {
 	int64_t		 rowid;
 	int64_t		 attachmentid;
@@ -137,20 +124,18 @@ struct sbk_attachment {
 
 SIMPLEQ_HEAD(sbk_attachment_list, sbk_attachment);
 
-struct sbk_mms {
-	int		 id;
+struct sbk_message {
 	char		*address;
-	uint64_t	 date_recv;
-	uint64_t	 date_sent;
-	int		 thread;
+	uint64_t	 time_sent;
+	uint64_t	 time_recv;
 	int		 type;
-	char		*body;
-	int		 nattachments;
+	int		 thread;
+	char		*text;
 	struct sbk_attachment_list *attachments;
-	SIMPLEQ_ENTRY(sbk_mms) entries;
+	SIMPLEQ_ENTRY(sbk_message) entries;
 };
 
-SIMPLEQ_HEAD(sbk_mms_list, sbk_mms);
+SIMPLEQ_HEAD(sbk_message_list, sbk_message);
 
 struct sbk_thread {
 	uint64_t	 id;
@@ -177,18 +162,13 @@ void		 sbk_free_frame(Signal__BackupFrame *);
 void		 sbk_free_file(struct sbk_file *);
 
 struct sbk_attachment_list *sbk_get_all_attachments(struct sbk_ctx *);
-struct sbk_attachment_list *sbk_get_attachments_for_mms(struct sbk_ctx *, int);
 struct sbk_attachment_list *sbk_get_attachments_for_thread(struct sbk_ctx *,
 		    int);
 void		 sbk_free_attachment_list(struct sbk_attachment_list *);
 
-struct sbk_sms_list *sbk_get_smses(struct sbk_ctx *, int);
-void		 sbk_free_sms_list(struct sbk_sms_list *);
-
-struct sbk_mms_list *sbk_get_mmses(struct sbk_ctx *, int);
-int		 sbk_get_mms_attachments(struct sbk_ctx *, struct sbk_mms *);
-int		 sbk_get_long_message(struct sbk_ctx *, struct sbk_mms *);
-void		 sbk_free_mms_list(struct sbk_mms_list *);
+struct sbk_message_list *sbk_get_all_messages(struct sbk_ctx *);
+struct sbk_message_list *sbk_get_messages_for_thread(struct sbk_ctx *, int);
+void		 sbk_free_message_list(struct sbk_message_list *);
 
 int		 sbk_get_contact(struct sbk_ctx *, const char *, char **,
 		    char **);

commit f84101b4f211f0b94fc283626344c69bb9265361

Plug memory leaks in the messages command
Introduced in commit 9aecccc4fa395bf4c6edb717eae6255257440ce4. Found by
Valgrind.

cmd-messages.c
@@ -270,6 +270,8 @@ text_write_message(struct sbk_ctx *ctx, FILE *fp, struct sbk_message *msg)
 	else
 		fputs("\n", fp);
 
+	freezero_string(name);
+	freezero_string(phone);
 	return 0;
 }
 