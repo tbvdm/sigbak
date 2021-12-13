e6d23b2d48b7a3120337ec0ebc5741e2779d0a51
Tim van der Molen <tim@kariliq.nl>
2021-12-13 17:56:00 +0100

    Support Solaris, for 'tis the season to be jolly

--- config.h
+++ config.h
@@ -139,6 +139,13 @@
 /* Define if you have musl >= 1.2.2. */
 /* #define HAVE_REALLOCARRAY */
 
+#elif defined(__sun)
+
+#define HAVE_ASPRINTF
+#define HAVE_ERR
+#define HAVE_FOPEN_X_MODE
+#define HAVE_GETPROGNAME
+
 #endif
 
 #include <openssl/opensslv.h>
