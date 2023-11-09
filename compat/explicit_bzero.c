b15790c98ec2ee0d877f1f5c177a920a4af3f7bc
Tim van der Molen <tim@kariliq.nl>
2023-11-09 15:44:16 +0100

    Adjust explicit_bzero() compat for LibreSSL
    Starting with version 3.8.1, LibreSSL no longer exports internal compat
    symbols, so its explicit_bzero() implementation is no longer visible.
    
    See also commit 04edf407fb07c556de664651621604fd58376087.
    
    This fixes the build on macOS.

--- compat/explicit_bzero.c
+++ compat/explicit_bzero.c
@@ -7,7 +7,8 @@
 
 #include <openssl/opensslv.h>
 
-#if !defined(HAVE_EXPLICIT_BZERO) && !defined(LIBRESSL_VERSION_NUMBER)
+#if !defined(HAVE_EXPLICIT_BZERO)
+#if !defined(LIBRESSL_VERSION_NUMBER) || LIBRESSL_VERSION_NUMBER >= 0x3080100fL
 
 #include <openssl/crypto.h>
 
@@ -17,4 +18,5 @@ explicit_bzero(void *buf, size_t len)
 	OPENSSL_cleanse(buf, len);
 }
 
+#endif
 #endif
